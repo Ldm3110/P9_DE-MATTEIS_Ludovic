bf4d089f041b2cd37c5f5b8ad0a89573f4d7ddda
Ldm3110 <ludovic.dematteis@icloud.com>
2021-11-05 00:19:34 +0100

    add modify and delete ticket function

--- LITReview/Flux/views.py
+++ LITReview/Flux/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from Flux import forms
+from Flux.models import Ticket
 
 
 @login_required
@@ -11,7 +12,8 @@ def homepage_view(request):
 
 @login_required
 def my_flux_view(request):
-    return render(request, 'flux/my_flux.html')
+    my_ticket = Ticket.objects.filter(user=request.user)
+    return render(request, 'flux/my_flux.html', context={'tickets': my_ticket})
 
 
 @login_required
@@ -27,3 +29,25 @@ def create_ticket(request):
 
     return render(request, 'flux/create_ticket.html', context={'form': form})
 
+
+@login_required
+def modify_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+    edit_ticket = forms.TicketForm(instance=ticket)
+    if request.method == 'POST':
+        edit_ticket = forms.TicketForm(request.POST, request.FILES, instance=ticket)
+        if edit_ticket.is_valid():
+            edit_ticket.save()
+            return redirect('my-flux')
+
+    return render(request, 'flux/modify_ticket.html', context={'form': edit_ticket})
+
+
+@login_required
+def delete_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+    ticket.image.delete()
+    ticket.delete()
+    message = f"Le ticket '{ticket.title}' a bien été supprimé"
+
+    return render(request, 'flux/my_flux.html', context={'message': message})
