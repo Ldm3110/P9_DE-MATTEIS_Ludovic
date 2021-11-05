c9345169213cf6829ff94cf896235106321b1c63
Ldm3110 <ludovic.dematteis@icloud.com>
2021-11-05 23:39:16 +0100

    creation of the display of user and subscriber tickets in the general flow

--- LITReview/Flux/views.py
+++ LITReview/Flux/views.py
@@ -1,19 +1,40 @@
+from itertools import chain
+
 from django.contrib.auth.decorators import login_required
+from django.db.models import Value, CharField, Q
 from django.shortcuts import render, redirect, get_object_or_404
 
+from Abonnements.models import UserFollows
 from Flux import forms
-from Flux.models import Ticket
+from Flux.models import Ticket, Review
 
 
 @login_required
 def homepage_view(request):
-    return render(request, 'flux/homepage.html')
+    user_tickets = get_users_viewable_tickets(request, request.user)
+    user_tickets = user_tickets.annotate(content_type=Value('TICKET', CharField()))
+
+    tickets = sorted(
+        user_tickets,
+        key=lambda post: post.time_created,
+        reverse=True
+    )
+
+    context = {'tickets': tickets}
+    return render(request, 'flux/homepage.html', context)
 
 
 @login_required
 def my_flux_view(request):
     my_ticket = Ticket.objects.filter(user=request.user)
-    return render(request, 'flux/my_flux.html', context={'tickets': my_ticket})
+    my_review = Review.objects.filter(user=request.user)
+
+    my_flux = sorted(chain(my_ticket, my_review),
+                     key=lambda post: post.time_created,
+                     reverse=True
+                     )
+
+    return render(request, 'flux/my_flux.html', context={'tickets': my_flux})
 
 
 @login_required
@@ -51,3 +72,32 @@ def delete_ticket(request, ticket_id):
     message = f"Le ticket '{ticket.title}' a bien été supprimé"
 
     return render(request, 'flux/my_flux.html', context={'message': message})
+
+
+@login_required
+def create_review(request):
+    pass
+
+
+@login_required
+def modify_review(request):
+    pass
+
+
+@login_required
+def delete_review(request):
+    pass
+
+
+def get_users_viewable_reviews(request, user):
+    reviews = Review.objects.filter(id=user.id)
+
+    return reviews
+
+
+def get_users_viewable_tickets(request, user):
+    followed_by = UserFollows.objects.filter(user=user)
+    tickets = Ticket.objects.filter(Q(user=user) |
+                                    Q(user__in=[user.followed_user for user in followed_by]))
+
+    return tickets
