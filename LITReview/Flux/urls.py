136893cfd5f49a21a0103fd9ee156af057bc7d58
Ldm3110 <ludovic.dematteis@icloud.com>
2021-11-07 16:41:57 +0100

    add modify and delete review urls

--- LITReview/Flux/urls.py
+++ LITReview/Flux/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create_ticket/', views.create_ticket, name='create-ticket'),
     path('modify_ticket/<int:ticket_id>', views.modify_ticket, name='modify-ticket'),
     path('delete_ticket/<int:ticket_id>', views.delete_ticket, name='delete-ticket'),
-    path('create_review/<int:ticket_id>', views.create_review, name='create-review')
+    path('create_review/<int:ticket_id>', views.create_review, name='create-review'),
+    path('modify_review/<int:review_id>', views.modify_review, name='modify-review'),
+    path('delete_review/<int:review_id>', views.delete_review, name='delete-review'),
 ]
