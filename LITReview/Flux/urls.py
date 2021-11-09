d75f3379008d897d9cfbc048373b833771829813
Ldm3110 <ludovic.dematteis@icloud.com>
2021-11-09 17:39:48 +0100

    update urls

--- LITReview/Flux/urls.py
+++ LITReview/Flux/urls.py
@@ -4,11 +4,14 @@ from Flux import views
 
 urlpatterns = [
     path('', views.homepage_view, name='homepage'),
-    path('my_flux/', views.my_flux_view, name='my-flux'),
     path('create_ticket/', views.create_ticket, name='create-ticket'),
-    path('modify_ticket/<int:ticket_id>', views.modify_ticket, name='modify-ticket'),
-    path('delete_ticket/<int:ticket_id>', views.delete_ticket, name='delete-ticket'),
     path('create_review/<int:ticket_id>', views.create_review, name='create-review'),
-    path('modify_review/<int:review_id>', views.modify_review, name='modify-review'),
-    path('delete_review/<int:review_id>', views.delete_review, name='delete-review'),
+    path('create_ticket_review/', views.ticket_and_review, name='ticket-review'),
+
+    path('my_flux/', views.my_flux_view, name='my-flux'),
+    path('my_flux/modify_ticket/<int:ticket_id>', views.modify_ticket, name='modify-ticket'),
+    path('my_flux/delete_ticket/<int:ticket_id>', views.delete_ticket, name='delete-ticket'),
+    path('my_flux/modify_review/<int:review_id>', views.modify_review, name='modify-review'),
+    path('my_flux/delete_review/<int:review_id>', views.delete_review, name='delete-review'),
+
 ]
